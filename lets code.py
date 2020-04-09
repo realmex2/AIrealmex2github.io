4159364dbc7120f9d36d988a7d2b582ac5cf9c2b
realmex2 <63112840+realmex2@users.noreply.github.com>
2020-04-09 23:02:23 +0530

    Add files via upload

---
+++ lets code.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#(ol(.html>
+
+#1
+mylist1=[1,2,3,4]
+print(mylist1)
+
+
+# In[11]:
+
+
+mylist2=['n','i','c','k']
+
+
+# In[12]:
+
+
+print(mylist2)
+
+
+# In[13]:
+
+
+print(mylist1+mylist2)
+
+
+# In[20]:
+
+
+#2
+
+mytupple=("my","thing")
+print(mytupple[:])
+
+
+# In[19]:
+
+
+#3
+
+mydict={"error":"system","warning":"program"}
+del mydict["error"]
+print(mydict)
+
+
+# In[ ]:
+
+
+
+
